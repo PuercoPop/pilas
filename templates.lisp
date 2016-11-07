f9e300af53269b3563f8d60755ef5881753b9152
Javier Olaechea <pirata@gmail.com>
2016-11-07 15:27:34 -0500

    Define NAVIGATION-BAR tag before it is used

--- templates.lisp
+++ templates.lisp
@@ -1,5 +1,11 @@
 (in-package "PILAS")
 
+(deftag navigation-bar (control attrs)
+  (declare (ignore control attrs))
+  '(:ul
+    (:li (:a :href "/create/" "Create new article"))
+    (:li (:a :href "/random/" "Random article"))))
+
 (defmacro with-page ((&key title) &body body)
   `(with-html-string
      (:doctype)
@@ -8,12 +14,6 @@
        (:title ,title))
       (:body (navigation-bar)
        ,@body))))
-
-(deftag navigation-bar (control attrs)
-  (declare (ignore control attrs))
-  '(:ul
-    (:li (:a :href "/create/" "Create new article"))
-    (:li (:a :href "/random/" "Random article"))))
 
 (defun show-entry (entry)
   (with-page (:title (title entry))
