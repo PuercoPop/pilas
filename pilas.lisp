1ed79281f2a662fae1811203f3255c160d1ff77a
Javier Olaechea <pirata@gmail.com>
2016-11-10 16:27:57 -0500

    Entries are now versioned
    - Old entries are kept in +OLD-ENTRIES-DIRECTORY+
    - LOAD-ENTRY updated to be able to load old entries
    - DELETE-ENTRY now walks the ancestor tree and deletes all entries
    - The empty string is used when an entry has no ancestor
    - Helper previous-entry added

--- pilas.lisp
+++ pilas.lisp
@@ -15,13 +15,18 @@ Design:
 (defparameter +wiki-root+ (asdf/system:system-relative-pathname :pilas #P"wiki/")
   "The top level directory where the wiki database lives.")
 
+(defparameter +old-entries-directory+ (asdf/system:system-relative-pathname :pilas #P"wiki/old/"))
+
 (defclass entry ()
   ((title :initarg :title
           :reader title
           :documentation "The title of the entry")
    (content :initarg :content
             :reader content
-            :documentation "The textual content of the entry in wiki-text.")))
+            :documentation "The textual content of the entry in wiki-text.")
+   (previous-checksum :initform ""
+                      :accessor previous-checksum
+                      :documentation "The sha256 of the previously *saved* version of the entry.")))
 
 (defun make-entry (title content)
   (make-instance 'entry :title title :content content))
@@ -78,17 +83,55 @@ Design:
             value))))
 
 (defun save-entry (entry)
+  (when-let (previous-entry (find-entry-by-title (title entry)))
+    (rename-file (merge-pathnames (compute-digest-for-entry previous-entry) +wiki-root+)
+                 (merge-pathnames (compute-digest-for-entry previous-entry) +old-entries-directory+))
+    (setf (previous-checksum entry)
+          (compute-digest-for-entry previous-entry)))
+
   (let ((*print-readably* t))
     (with-open-file (out (entry-pathname entry) :direction :output :external-format :utf-8)
       (write (entry-plist entry) :stream out))))
 
 (defun load-entry (digest)
-  (with-open-file (in (merge-pathnames digest +wiki-root+) :direction :input :external-format :utf-8)
-    (let ((*package* (find-package "PILAS")))
-      (plist-entry (read in)))))
+  "Return an ENTRY for the corresponding DIGEST."
+  ;; First look for the entry in +wiki-root+, if not found look for it in
+  ;; +old-entries-directory+
+  (flet ((probe-files (&rest pathnames)
+           "Return the first pathname that exists, nil pathname exists"
+           (loop :for pathname :in pathnames
+                 :when (probe-file pathname)
+                   :return pathname)))
+    (let ((entry-pathname (merge-pathnames digest +wiki-root+))
+          (fallback-pathname (merge-pathnames digest +old-entries-directory+)))
+      (with-open-file (in (probe-files entry-pathname fallback-pathname) :direction :input :external-format :utf-8)
+        (let ((*package* (find-package "PILAS")))
+          (plist-entry (read in)))))))
 
+;; TODO: Merge delete-entry and delete-ancestor-entry
 (defun delete-entry (entry)
-  (delete-file (entry-pathname entry)))
+  "Delete the entry and all of its ancestors."
+  ;; XXX: Should we only delete the most recent one and move its ancestor back
+  ;; to the +WIKI-ROOT+ folder?
+  (delete-file (entry-pathname entry))
+
+  (let ((old-id (previous-checksum entry)))
+    (unless (string= old-id "")
+      (delete-ancestor-entry old-id))))
+
+(defun delete-ancestor-entry (digest)
+  (let ((entry (load-entry digest)))
+    (delete-file (merge-pathnames digest +old-entries-directory+))
+
+    (let ((old-id (previous-checksum entry)))
+      (unless (string= old-id "")
+        (delete-ancestor-entry old-id)))))
+
+(defun previous-entry (entry)
+  "Load the ENTRY's ancestor."
+  (let ((old-id (previous-checksum entry)))
+    (unless (string= old-id "")
+      (load-entry old-id))))
 
 (defun list-entry-titles ()
   "Return a list of the title of every entry in the wiki."
