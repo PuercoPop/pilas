6035a3b8c6b2215c5d5e0c2e213066ac6729fcd5
Javier Olaechea <pirata@gmail.com>
2016-11-25 18:49:29 -0500

    FORM to ENTRY and vice versa helper functions

--- web/forms.lisp
+++ web/forms.lisp
@@ -4,6 +4,19 @@
   ((title :text :validator (clavier:not-blank))
     (content :text)))
 
+(defun entry->form (entry)
+  (make-entry-form :title (title entry) :content (content entry)))
+
+(defun form->entry (form)
+  (assert (validate form) nil "Form data invalid.")
+  (let* ((form-data (form-data form))
+         (entry-title (cdr (assoc 'title form-data)))
+         (entry-content (cdr (assoc 'content form-data))))
+    (if-let (entry (find-entry-by-title entry-title))
+      (progn (setf (slot-value entry 'title) entry-title
+                   (slot-value entry 'content) entry-content)
+             entry)
+      (make-entry entry-title entry-content))))
 (define-easy-handler (validate-entry-endpoint :uri "/validate-entry/") (title content)
   (let ((form (make-entry-form :title title :content content)))
     (multiple-value-bind (data errors)
